4f5241ad09e8b291c230f9aa5362a420690b2333
kyle799 <kyle@15kmr.com>
2022-11-25 11:10:48 -0700

    menu bar done

--- Gui/main.py
+++ Gui/main.py
@@ -6,10 +6,11 @@ import time
 
 global buttonForegroundColor
 global buttonBackgroundColor
+global ft
 
 buttonForegroundColor ="#000000"
 buttonBackgroundColor = "#f0f0f0"
-
+#ft = tkFont.Font(family='Times',size=10)
 class App:
     def __init__(self, root):
         root.title("7D2D Server Manager")
@@ -19,51 +20,59 @@ class App:
         screenheight = root.winfo_screenheight()
         alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
         root.geometry(alignstr)
-        root.resizable(width=False, height=False)
         ft = tkFont.Font(family='Times',size=10)
 
-        button1=tk.Button(root, font=ft, foreground=buttonForegroundColor, background=buttonBackgroundColor, justify="center", text="Main", command=self.button1_command)
-        button1.place(x=0,y=0,width=70,height=25)
+        pageMenu = tk.Frame(root)
+        pageMenu.columnconfigure(0, weight=1)
 
-        button2=tk.Button(root, font=ft, foreground=buttonForegroundColor, background=buttonBackgroundColor, justify="center", text="Config", command=self.button2_command)
-        button2.place(x=70,y=0,width=70,height=25)
+        button1Main=tk.Button(pageMenu, font=ft, foreground=buttonForegroundColor, background=buttonBackgroundColor, justify="center", text="Main", command=self.button1Main_command)
+        button1Main.grid(row=0, column=0, sticky='w', padx=5,)
 
-        button3=tk.Button(root, font=ft, foreground=buttonForegroundColor, background=buttonBackgroundColor, justify="center",text="Perk Editor", command=self.button3_command)
-        button3.place(x=140,y=0,width=70,height=25)
+        button2Config=tk.Button(pageMenu, font=ft, foreground=buttonForegroundColor, background=buttonBackgroundColor, justify="center", text="Config", command=self.button2Config_command)
+        button2Config.grid(row=0, column=1, sticky='w', padx=5)
 
-        button4=tk.Button(root, font=ft, foreground=buttonForegroundColor, background=buttonBackgroundColor, justify="center", text="Button", command=self.button4_command)
-        button4.place(x=210,y=0,width=70,height=25)
+        button3PerkEditor=tk.Button(pageMenu, font=ft, foreground=buttonForegroundColor, background=buttonBackgroundColor, justify="center", text="Perk Editor", command=self.button3PerkEditor_command)
+        button3PerkEditor.grid(row=0, column=2, sticky='w', padx=5)
 
-        button5=tk.Button(root, font=ft, foreground=buttonForegroundColor, background=buttonBackgroundColor, justify="center",text="Import Configuration",command=self.button5_command)
-        button5.place(x=430,y=0,width=170,height=25)
+        button4Button=tk.Button(pageMenu, font=ft, foreground=buttonForegroundColor, background=buttonBackgroundColor, justify="center", text="Button", command=self.button4Button_command)
+        button4Button.grid(row=0, column=3, sticky='w', padx=5)
 
-        input1=tk.Entry(root, font=ft, borderwidth="1px",foreground=buttonForegroundColor, background=buttonBackgroundColor, justify="left",)
-        #input1["text"] = "Entry"
-        #input1["show"] = "servername"
-        input1.place(x=0,y=50,width=424,height=37)
+        button5ImportConfiguration=tk.Button(pageMenu, font=ft, foreground=buttonForegroundColor, background=buttonBackgroundColor, justify="center", text="Import Configuration", command=self.button5ImportConfiguration_command)
+        button5ImportConfiguration.grid(row=0, column=4, sticky= 'e', padx=5)
 
-        button6=tk.Button(root, font=ft, foreground=buttonForegroundColor, background=buttonBackgroundColor, justify="center", text="Button", command=self.button6_command)
-        button6.place(x=430,y=50,width=83,height=30)
+        pageMenu.pack()
 
-    def button1_command(self):
+
+
+#add to mainpage 
+        #input1=tk.Entry(root, font=ft, borderwidth="1px",foreground=buttonForegroundColor, background=buttonBackgroundColor, justify="left")
+        #input1.pack()
+
+        #button6Button=tk.Button(root, font=ft, foreground=buttonForegroundColor, background=buttonBackgroundColor, justify="center", text="Button", command=self.button6Button_command)
+        #button6Button.place(x=430,y=50,width=83,height=30)
+
+
+
+
+    def button1Main_command(self):
         print(f"click main screen")
 
 
-    def button2_command(self):
+    def button2Config_command(self):
         print(f"click config")
 
 
-    def button3_command(self):
+    def button3PerkEditor_command(self):
         print(f"click perk edit")
 
 
-    def button4_command(self):
+    def button4Button_command(self):
         print(f"button 4")
     
-    def button5_command(self):
+    def button5ImportConfiguration_command(self):
         print(f"import config")
 
-    def button6_command(self):
+    def button6Button_command(self):
         print(f"Changed servername to {self.input1.get('1.0', tk.END)} ")
 
 if __name__ == "__main__":
