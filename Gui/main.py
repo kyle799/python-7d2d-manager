771145331ab2648d3cc828d87b3825e4c1bc0c0a
kyle799 <kyle@15kmr.com>
2022-11-25 10:30:35 -0700

    Refactor buttons, add buttonForegrounColor and buttonBackgroundColor

--- Gui/main.py
+++ Gui/main.py
@@ -4,6 +4,11 @@ import tkinter.font as tkFont
 
 import time
 
+global buttonForegroundColor
+global buttonBackgroundColor
+
+buttonForegroundColor ="#000000"
+buttonBackgroundColor = "#f0f0f0"
 
 class App:
     def __init__(self, root):
@@ -17,68 +22,28 @@ class App:
         root.resizable(width=False, height=False)
         ft = tkFont.Font(family='Times',size=10)
 
-
-        button1=tk.Button(root)
-        button1["bg"] = "#f0f0f0"
-        button1["font"] = ft
-        button1["fg"] = "#000000"
-        button1["justify"] = "center"
-        button1["text"] = "Main"
+        button1=tk.Button(root, font=ft, foreground=buttonForegroundColor, background=buttonBackgroundColor, justify="center", text="Main", command=self.button1_command)
         button1.place(x=0,y=0,width=70,height=25)
-        button1["command"] = self.button1_command
 
-        button2=tk.Button(root)
-        button2["bg"] = "#f0f0f0"
-        button2["font"] = ft
-        button2["fg"] = "#000000"
-        button2["justify"] = "center"
-        button2["text"] = "Config"
+        button2=tk.Button(root, font=ft, foreground=buttonForegroundColor, background=buttonBackgroundColor, justify="center", text="Config", command=self.button2_command)
         button2.place(x=70,y=0,width=70,height=25)
-        button2["command"] = self.button2_command
 
-        button3=tk.Button(root)
-        button3["bg"] = "#f0f0f0"
-        button3["font"] = ft
-        button3["fg"] = "#000000"
-        button3["justify"] = "center"
-        button3["text"] = "Perk Editor"
+        button3=tk.Button(root, font=ft, foreground=buttonForegroundColor, background=buttonBackgroundColor, justify="center",text="Perk Editor", command=self.button3_command)
         button3.place(x=140,y=0,width=70,height=25)
-        button3["command"] = self.button3_command
 
-        button4=tk.Button(root)
-        button4["bg"] = "#f0f0f0"
-        button4["font"] = ft
-        button4["fg"] = "#000000"
-        button4["justify"] = "center"
-        button4["text"] = "Button"
+        button4=tk.Button(root, font=ft, foreground=buttonForegroundColor, background=buttonBackgroundColor, justify="center", text="Button", command=self.button4_command)
         button4.place(x=210,y=0,width=70,height=25)
-        button4["command"] = self.button4_command
 
-        button5=tk.Button(root)
-        button5["bg"] = "#f0f0f0"
-        button5["font"] = ft
-        button5["fg"] = "#000000"
-        button5["justify"] = "center"
-        button5["text"] = "Import configuration"
+        button5=tk.Button(root, font=ft, foreground=buttonForegroundColor, background=buttonBackgroundColor, justify="center",text="Import Configuration",command=self.button5_command)
         button5.place(x=430,y=0,width=170,height=25)
-        button5["command"] = self.button5_command
 
-        input1=tk.Entry(root)
-        input1["borderwidth"] = "1px"
-        input1["font"] = ft
-        input1["fg"] = "#333333"
-        input1["justify"] = "left"
-        input1["text"] = "Entry"
-        input1["show"] = "servername"
+        input1=tk.Entry(root, font=ft, borderwidth="1px",foreground=buttonForegroundColor, background=buttonBackgroundColor, justify="left",)
+        #input1["text"] = "Entry"
+        #input1["show"] = "servername"
         input1.place(x=0,y=50,width=424,height=37)
 
-        button6=tk.Button(root)
-        button6["bg"] = "#f0f0f0"
-        button6["fg"] = "#000000"
-        button6["justify"] = "center"
-        button6["text"] = "Button"
+        button6=tk.Button(root, font=ft, foreground=buttonForegroundColor, background=buttonBackgroundColor, justify="center", text="Button", command=self.button6_command)
         button6.place(x=430,y=50,width=83,height=30)
-        button6["command"] = self.button6_command
 
     def button1_command(self):
         print(f"click main screen")
@@ -99,7 +64,7 @@ class App:
         print(f"import config")
 
     def button6_command(self):
-        print(f"Changed servername to ")
+        print(f"Changed servername to {self.input1.get('1.0', tk.END)} ")
 
 if __name__ == "__main__":
     root = tk.Tk()
